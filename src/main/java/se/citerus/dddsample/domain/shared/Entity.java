221a2bf9546c39d183b3dd0e6de85eb62ee46be0
Danil <danil@vast.com>
2018-04-01 14:55:25 -0400

    Replaces EasyMock with Mockito.

--- src/main/java/se/citerus/dddsample/domain/shared/Entity.java
+++ src/main/java/se/citerus/dddsample/domain/shared/Entity.java
@@ -2,6 +2,7 @@ package se.citerus.dddsample.domain.shared;
 
 /**
  * An entity, as explained in the DDD book.
+ *  
  */
 public interface Entity<T> {
 
@@ -9,7 +10,7 @@ public interface Entity<T> {
      * Entities compare by identity, not by attributes.
      *
      * @param other The other entity.
-     * @return true if the identities are the same, regardles of other attributes.
+     * @return true if the identities are the same, regardless of other attributes.
      */
     boolean sameIdentityAs(T other);
 
